a65e152e7d4679f7c6fbf084e6a1992d7b2fbe9d
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-30 15:56:09 -0700

    added-map()-function

--- src/App.js
+++ src/App.js
@@ -6,34 +6,47 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchFild: '',
     };
-    console.log("1");
+    // console.log("1");
   }
   componentDidMount() {
-    console.log("3");
+    // console.log("3");
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((response) => response.json())
       .then((users) =>
         this.setState(
           () => {
             return { monsters: users };
-            
           },
-          
+
           () => {
             console.log(this.state);
           }
         )
-        
       );
-      
   }
 
   render() {
-    console.log("2");
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchFild);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          type="search"
+          className="search-box"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            const searchFild = event.target.value.toLowerCase()
+            
+            this.setState(() => {
+              return { searchFild };
+            });
+          }}
+        />
+        {/* Map function to monsters API */}
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
