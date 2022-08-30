b9c791efdc079c814178f83d95e80e82019b3d2d
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-30 15:03:01 -0700

    added-useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-class App extends Component{
-constructor(){
-  super();
-  this.state= {
-    name: 'Mike'
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+    console.log("1");
   }
-}
+  componentDidMount() {
+    console.log("3");
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+            
+          },
+          
+          () => {
+            console.log(this.state);
+          }
+        )
+        
+      );
+      
+  }
 
-  render(){
+  render() {
+    console.log("2");
     return (
       <div className="App">
-        {this.state.mosters.map((monster) => {
+        {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-    )
+          );
+        })}
+      </div>
+    );
   }
 }
-
 
 export default App;
