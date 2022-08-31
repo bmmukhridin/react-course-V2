008c23ff9577dfb9b8ab8811110f52079d4d8a12
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-31 13:24:06 -0700

    working-with-function-component

--- src/App.js
+++ src/App.js
@@ -4,46 +4,58 @@ import CardList from "./componets/card-list/card-list";
 import SearchBox from "./componets/search-box/search-box";
 ////////////
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchFild: "",
-    };
-  }
-  componentDidMount() {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
-  onSearchChange = (event) => {
-    const searchFild = event.target.value.toLowerCase();
-
-    this.setState(() => {
-      return { searchFild };
-    });
-  };
-  render() {
-    const { searchFild, monsters } = this.state;
-    const { onSearchChange } = this;
-    console.log("Render");
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchFild);
-    });
-    return (
-      <div className="App">
-        {/* Search Box input */}
-        <SearchBox filter={onSearchChange} />
-        {/* Map function to monsters API */}
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
+function App(){
+  return(
+    <div className="App">
+    {/* Search Box input */}
+    <SearchBox filter={onSearchChange} />
+    {/* Map function to monsters API */}
+    <CardList monsters={filteredMonsters} />
+  </div>
+  )
 }
+
+////////  Class Components  ////////////////
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchFild: "",
+//     };
+//   }
+//   componentDidMount() {
+//     fetch(`https://jsonplaceholder.typicode.com/users`)
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
+//   onSearchChange = (event) => {
+//     const searchFild = event.target.value.toLowerCase();
+
+//     this.setState(() => {
+//       return { searchFild };
+//     });
+//   };
+//   render() {
+//     const { searchFild, monsters } = this.state;
+//     const { onSearchChange } = this;
+//     console.log("Render");
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLowerCase().includes(searchFild);
+//     });
+//     return (
+//       <div className="App">
+//         {/* Search Box input */}
+//         <SearchBox filter={onSearchChange} />
+//         {/* Map function to monsters API */}
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
