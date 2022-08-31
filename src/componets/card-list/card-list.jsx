55c1cb4411ed86d51189270297f26a498dbcf0c0
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-30 22:20:30 -0700

    finished-with-style

--- src/componets/card-list/card-list.jsx
+++ src/componets/card-list/card-list.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./card-list.styles.css"
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
@@ -8,7 +8,7 @@ class CardList extends Component {
         {monsters.map((monster) => {
           return (
             <div className="card-container" key={monster.id}>
-              <img alt={`monster ${monster.name}`}src={`https://robohash.org/${monster.id+1}?set=set2$size=180x180`}/>
+              <img alt={`monster ${monster.name}`}src={`https://robohash.org/${monster.id+1}?set=set2&size=180x180`}/>
               <h2>{monster.name}</h2>
               <p>{monster.email}</p>
             </div>
